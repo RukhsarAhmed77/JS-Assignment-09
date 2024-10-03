fdcb602012a0393116492cd41f351a5d03d5dc85
Rukhsar Ahmed <rukhsarnaich0@gmail.com>
2024-10-03 21:15:44 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+//JS Assignment 09
+
+// var series = [1, 2, 3, 4, 6, 7, 8, 9, 10];
+// var n = 10;
+
+// var expectedSum = (n * (n + 1)) / 2;
+
+// var actualSum = 0;
+// for (var i = 0; i < series.length; i++) {
+//     actualSum = actualSum + series[i];
+// }
+
+// var missingNumber = expectedSum - actualSum;
+
+// console.log("The missing number is: " + missingNumber);
